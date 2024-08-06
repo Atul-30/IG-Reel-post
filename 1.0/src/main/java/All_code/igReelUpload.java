d2a9b8c983113c0e45477e401b014756f8351acf
Atul <atul.tumbare@gmail.com>
2024-08-06 14:41:56 +0530

    All done

--- 1.0/src/main/java/All_code/igReelUpload.java
+++ 1.0/src/main/java/All_code/igReelUpload.java
@@ -1,91 +1,132 @@
 package All_code;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.time.Duration;
-import java.util.List;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.sl.usermodel.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class igReelUpload {
 
-//	static WebDriver driver=new ChromeDriver();
-//	static Duration timeout = Duration.ofSeconds(200);
-//    static WebDriverWait wait=new WebDriverWait(driver,timeout);
-    static Boolean check= true;
+    static WebDriver driver;
+    static Duration timeout = Duration.ofSeconds(200);
+    static WebDriverWait wait;
+    static Boolean check = true;
     static int num;
-	public static void main(String[] args) throws InterruptedException, IOException {
-//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\atult\\git\\IG-Reel-post\\1.0\\src\\main\\resources\\chromedriver.exe");
-//		loginInstagram();
-		fetchReelNameAndCaption();
-	}
-	
-	public static void loginInstagram() throws InterruptedException
-	{
-//        driver.get("https://www.instagram.com/");
-//        driver.manage().window().maximize();
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='username']"))).sendKeys("insta_store_7");
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='password']"))).sendKeys("Atul@1234");
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']"))).click();
-	}
-	
-	public static void fetchReelNameAndCaption() throws IOException
-	{
-		
-		FileInputStream fis = new FileInputStream(new File("C:\\Users\\atult\\git\\IG-Reel-post\\1.0\\src\\main\\resources\\Task list.xlsx"));
+    static String videoLink;
+    static String caption;
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+            System.setProperty("webdriver.gecko.driver", "C:\\Users\\atult\\git\\IG-Reel-post\\1.0\\src\\main\\resources\\mozila.exe");
+
+//            driver = new ChromeDriver();
+            driver = new FirefoxDriver();
+            wait = new WebDriverWait(driver, timeout);
+            loginInstagram();
+            fetchReelNameAndCaption();
+            selectReelandShareReel();
+            Thread.sleep(5000);
+            driver.quit();
+    }
+
+    public static void loginInstagram() throws InterruptedException {
+        driver.get("https://www.instagram.com/");
+        driver.manage().window().maximize();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='username']"))).sendKeys("insta_store_7");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='password']"))).sendKeys("Atul@1234");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']"))).click();
+    }
+
+    public static void fetchReelNameAndCaption() throws IOException {
+        FileInputStream fis = new FileInputStream(new File("C:\\Users\\atult\\git\\IG-Reel-post\\1.0\\src\\main\\resources\\Task list.xlsx"));
         Workbook workbook = new XSSFWorkbook(fis);
         Sheet sheet = workbook.getSheet("Sheet1");
-        Row rowLink = sheet.getRow(1);
-		do
-		{
-			num++;
-			rowLink = sheet.getRow(num);
-			Cell cellStatus = rowLink.getCell(3);
-	        String status = cellStatus.getStringCellValue();
-			if(!status.equals("Done"))
-			{
-				check=false;
-			}
-		}
-		while(check==true);
-		
+
+        Row rowLink;
+        do {
+            num++;
+            rowLink = sheet.getRow(num);
+            Cell cellStatus = rowLink.getCell(3);
+            String status = cellStatus.getStringCellValue();
+            if (!status.equals("Done")) {
+                check = false;
+            }
+        } while (check);
+
         Cell cellLink = rowLink.getCell(1);
-        String videoLink = cellLink.getStringCellValue();
+        videoLink = cellLink.getStringCellValue();
         System.out.println(videoLink);
-        
+
         Cell cellCaption = rowLink.getCell(2);
-        String caption = cellCaption.getStringCellValue();
+        caption = cellCaption.getStringCellValue();
         System.out.println(caption);
-        
+
         Cell cell = rowLink.getCell(3);
         cell.setCellValue("Done");
 
         // Write the changes to the file
-        try (FileOutputStream fos = new FileOutputStream("C:\\Users\\atult\\git\\IG-Reel-post\\1.0\\src\\main\\resources\\Task list.xlsx")) 
-        {
+        try (FileOutputStream fos = new FileOutputStream("C:\\Users\\atult\\git\\IG-Reel-post\\1.0\\src\\main\\resources\\Task list.xlsx")) {
             workbook.write(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        
-	    catch (IOException e) 
-        {
-	        e.printStackTrace();
-	    }
+
         workbook.close();
         fis.close();
-	}
+    }
+
+    public static void selectReelandShareReel() throws InterruptedException {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Create']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Post']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Select from computer']"))).click();
+        
+        try {
+            Robot robot = new Robot();
+            robot.setAutoDelay(2000);
+
+            StringSelection stringSelection = new StringSelection(videoLink);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+
+            // Paste the string (simulate Ctrl+V)
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+
+            // Press Enter (simulate pressing the Enter key)
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='OK']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[5]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div/div[1]/div/div[2]/div/button/div"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='9:16']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Next']"))).click();
+        Thread.sleep(3000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Next']"))).click();
+        Thread.sleep(3000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@aria-label='Write a caption...']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@aria-label='Write a caption...']"))).sendKeys(caption);
+        System.out.println(caption);
+    }
+        
+    
 }
